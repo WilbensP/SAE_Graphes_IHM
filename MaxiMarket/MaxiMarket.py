1365ed7c113049a76b13f6309f3175ead5fc1f21
Wilbens Polisca <wpolisca11@gmail.com>
2025-06-10 13:44:48 +0200

    etape 2,3 de l'application 1 fini : adapter avec exemple 4 (avec os et sans classe scene l'image n'est pas de bonne qualité)

--- MaxiMarket/MaxiMarket.py
+++ MaxiMarket/MaxiMarket.py
@@ -1,32 +1,81 @@
+import sys
+import os
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QGraphicsScene, QGraphicsView, QGraphicsItem, QGraphicsPixmapItem, QGraphicsLineItem
+from PyQt6.QtGui import QPen, QPixmap, QColor
+from PyQt6.QtCore import Qt
 
-#  charger l image
-chemin_image = "plan.jpg"
-plan_magasin = mpimg.imread(chemin_image)
+class Scene(QGraphicsScene):
+    def __init__(self):
+        super().__init__()
+        
+        self.nb_rangs = 24
+        self.nb_rayons = 47
+        
+        self.charger_plan()
+    
+    def charger_plan(self):
+        chemin_image = sys.path[0] + "/MaxiMarket/plan.jpg"
+        if os.path.exists(chemin_image):
+            # Charger l'image
+            self.plan = QPixmap(chemin_image)
+            self.image = QGraphicsPixmapItem(self.plan)
+            self.addItem(self.image)
+            self.setSceneRect(0, 0, self.plan.width(), self.plan.height())
+            
+            # Créer le quadrillage
+            self.creer_quadrillage()
+    
+    def creer_quadrillage(self):
+        largeur_image = self.plan.width()
+        hauteur_image = self.plan.height()
+        hauteur_case = hauteur_image / self.nb_rangs
+        largeur_case = largeur_image / self.nb_rayons
+        
+        # Crayon pour quadrillage
+        crayon_rouge = QPen(QColor(255, 0, 0))
+        crayon_rouge.setWidth(1)
+        
+        # Lignes horizontales
+        for rang in range(self.nb_rangs + 1):
+            y_line = rang * hauteur_case
+            ligne = QGraphicsLineItem(0, y_line, largeur_image, y_line)
+            ligne.setPen(crayon_rouge)
+            self.addItem(ligne)
+        
+        # Lignes verticales
+        for rayon in range(self.nb_rayons + 1):
+            x_line = rayon * largeur_case
+            ligne = QGraphicsLineItem(x_line, 0, x_line, hauteur_image)
+            ligne.setPen(crayon_rouge)
+            self.addItem(ligne)
 
-# dimension image quadrillage
-nb_rangs, nb_rayons = 24, 47
-hauteur_image, largeur_image = plan_magasin.shape[0], plan_magasin.shape[1]
-hauteur_case = hauteur_image / nb_rangs
-largeur_case = largeur_image / nb_rayons
+class Rendu(QGraphicsView):
 
-# quadrillage rouge par dessus l image
-fig, ax = plt.subplots(figsize=(5, 5), dpi=100)
-ax.imshow(plan_magasin)
+    def __init__(self):
+        super().__init__()
+        
+        scene = Scene()
+        self.setScene(scene)
+        self.fitInView(scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
 
-# quadrillage rouge
-for rang in range(nb_rangs + 1):
-    y = rang * hauteur_case
-    ax.plot([0, largeur_image], [y, y], color='red', linewidth=0.5)
+class MaxiMarketApp(QMainWindow):
 
-for rayon in range(nb_rayons + 1):
-    x = rayon * largeur_case
-    ax.plot([x, x], [0, hauteur_image], color='red', linewidth=0.5)
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("MaxiMarket")
+        self.resize(800, 600)
+        
+        # Créer le rendu graphique
+        self.rendu = Rendu()
+        self.setCentralWidget(self.rendu)
 
-# on a retirer les axes
-ax.set_xticks([])
-ax.set_yticks([])
-plt.tight_layout()
-plt.show()
+def main():
+    app = QApplication(sys.argv)
+    window = MaxiMarketApp()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
